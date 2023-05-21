601c4217a954905da9543ad1f8a5be3106373468
Soumya Polisetty <soumyapolise@umass.edu>
2023-05-21 10:33:26 -0700

    Time: 1589 ms (75.00%), Space: 16.4 MB (25.00%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
@@ -2,20 +2,20 @@ class Solution:
     def punishmentNumber(self, n: int) -> int:
         def is_par(num, i, res):
             if num == '':
-                if i == res:
+                if i==res:
                     return True
                 else:
                     return False
             for j in range(1, len(num)+1):
                 if is_par(num[j:], i, res + int(num[:j])):
                     return True
-            
             return False
-                
-        substring_sum = 0    
+            
+        
+        substring_sum = 0
         for i in range(1, n+1):
-            square = i * i
+            square = i*i
             if is_par(str(square), i, 0):
                 substring_sum += square
-            
+        
         return substring_sum
