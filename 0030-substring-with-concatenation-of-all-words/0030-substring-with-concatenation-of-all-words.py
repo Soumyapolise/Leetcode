62705c5fd2ce5d002506b2405a764ea0ce671562
Soumya Polisetty <soumyapolise@umass.edu>
2023-06-04 14:54:24 -0700

    Time: 4866 ms (8.87%), Space: 16.7 MB (29.98%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        d_copy = {}
+        for word in words:
+            if word in d_copy:
+                d_copy[word] += 1
+            else:
+                d_copy[word] = 1
+        
+        res = []
+        n = len(words[0])
+        for i in range(len(s)):
+            d = d_copy.copy()
+            start = i
+            while s[i:i+n] in d:
+                d[s[i:i+n]] -= 1
+                if d[s[i:i+n]] == 0:
+                    d.pop(s[i:i+n])
+                i += n
+            
+            if not d:
+                res.append(start)
+        
+        return res
