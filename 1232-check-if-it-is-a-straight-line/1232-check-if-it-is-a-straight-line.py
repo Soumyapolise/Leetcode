5c5f55f9bf8c24bb620f866caf7eaf43fee9c737
Soumya Polisetty <soumyapolise@umass.edu>
2023-06-04 18:04:19 -0700

    Time: 73 ms (48.80%), Space: 16.7 MB (53.60%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -1,17 +1,7 @@
 class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-#         if (coordinates[1][0]-coordinates[0][0]) == 0:
-#             slope = "#"
-#         else:
-#             slope = (coordinates[1][1]-coordinates[0][1])/(coordinates[1][0]-coordinates[0][0])
-            
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:    
         for i in range(2, len(coordinates)):
-            if (coordinates[i][1]-coordinates[i-1][1])*(coordinates[1][0]-coordinates[0][0]) != (coordinates[i][0]-coordinates[i-1][0])*(coordinates[1][1]-coordinates[0][1]):
+            if (coordinates[i][1]-coordinates[i-1][1])*(coordinates[1][0]-coordinates[0][0]) != (coordinates[i][0]-coordinates[i-1][0])*(coordinates[1][1]-coordinates[0][1]): #cross multiplication, rather than handling case of denominator being zero using 'if' statements
                 return False
-            # if slope!="#":
-            #     if (coordinates[i][0]-coordinates[i-1][0]) == 0 or (coordinates[i][1]-coordinates[i-1][1])/(coordinates[i][0]-coordinates[i-1][0]) != slope:
-            #         return False
-            # elif (coordinates[i][0]-coordinates[i-1][0]) != 0:
-            #         return False
         
         return True
