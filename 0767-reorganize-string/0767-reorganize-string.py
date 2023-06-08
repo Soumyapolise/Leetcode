cac77f5483009acf6da218cbaffa4460347670e8
Soumya Polisetty <soumyapolise@umass.edu>
2023-06-08 12:58:24 -0700

    Time: 52 ms (32.93%), Space: 16.4 MB (5.82%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,31 +1,28 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         d = {}
-        res = []
         for ch in s:
             if ch in d:
                 d[ch] += 1
             else:
                 d[ch] = 1
-        
-        d = dict(sorted(d.items(), key = lambda x:x[1], reverse=True)) #sorted in decreasing order
-        max_count = list(d.values())[0]
+            
+        d = dict(sorted(d.items(), key = lambda x:x[1], reverse=True))
         max_char = list(d.keys())[0]
+        max_count = list(d.values())[0]
         
-        if max_count > (len(s)+1)//2: #getting rid of not possible arrangements
+        if max_count > (len(s)+1)//2:
             return ""
-              
-        for _ in range(max_count):
-            res += [max_char] #res = ["a", "a", "a", "a", "a"] if max_char = "a", max_count = 5  
+        
+        res = [max_char]*max_count
         i = 0
         for key, value in d.items():
-            if key == max_char: 
-                continue #since we've already added our max_char to res
+            if key == max_char:
+                continue
             for _ in range(value):
-                res[i] += key #just adding different chars next to our previous values in res. #Pigeonhole principle
-                if i != len(res)-1:
-                    i+=1
-                else:
-                    i=0
-                    
+                res[i] += key
+                i+=1
+                if i == len(res):
+                    i = 0
+        
         return "".join(res)
