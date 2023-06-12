9eacc22e5aa8b00653660634db81f98bc4d21056
Soumya Polisetty <soumyapolise@umass.edu>
2023-06-12 16:08:53 -0700

    Time: 61 ms (13.45%), Space: 16.7 MB (15.66%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,32 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        subsets = []
-        current = []
-        nums.sort()  # Sort the array to handle duplicates
-        self.backtrack(0, current, subsets, nums)
-        return subsets
-
-    def backtrack(self, startIndex, current, subsets, nums):
-        subsets.append(current[:])  # Add the current subset to the subsets list
-
-        for i in range(startIndex, len(nums)):
-            if i > startIndex and nums[i] == nums[i-1]:
-                continue
-
-            current.append(nums[i])  # Add the current element to the current subset
-            self.backtrack(i + 1, current, subsets, nums)  # Recursively call backtrack
-            current.pop()  # Remove the current element (backtrack)
-
+        res = [[]]
+        nums = sorted(nums)
         
-#         res = [[]]
-#         nums = sorted(nums)
+        for num in nums:
+            for i in range(len(res)):
+                res.append(res[i] + [num])
         
-#         for num in nums:
-#             for i in range(len(res)):
-#                 res.append(res[i] + [num])
+        d = {}
+        for arr in res:
+            d[tuple(arr)] = 1
         
-#         d = {}
-#         for arr in res:
-#             d[tuple(arr)] = 1
-        
-#         return list(d.keys())
+        return list(d.keys())
