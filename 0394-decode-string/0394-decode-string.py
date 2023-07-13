fce827242ba01a8c14efeae51f5e65dec823c05b
Soumya Polisetty <soumyapolise@umass.edu>
2023-07-12 21:47:17 -0700

    Time: 51 ms (25.59%), Space: 16.3 MB (77.21%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        res = ""
+        num = 0
+        
+        for ch in s:
+            if ch.isnumeric():
+                num = num*10 + int(ch)
+            elif ch == "[":
+                stack.append(res)
+                stack.append(num)
+                res = ""
+                num = 0
+            elif ch == "]":
+                count = stack.pop()
+                prev_string = stack.pop()
+                res = prev_string + count*res
+            else:
+                res += ch
+            
+        return res
+                
