295f53da66852e3527e5b322097f5f930b4d1793
Soumya Polisetty <soumyapolise@umass.edu>
2023-07-20 21:02:05 -0700

    Time: 1328 ms (36.26%), Space: 16.7 MB (52.88%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -4,16 +4,16 @@ class Solution:
         length = [1] * n
         count = [1] * n
         
-        for i in range(n):
-            for j in range(i):
+        for i in range(n): #4
+            for j in range(i): #3
                 if nums[j] < nums[i]:
                     if length[j] + 1 > length[i]:
-                        length[i] = length[j] + 1
-                        count[i] = 0
-                    if length[j] + 1 == length[i]:
-                        count[i] += count[j]
+                        length[i] = length[j] + 1 # [1,2,3,3,4]
+                        count[i] = 0 #[1,0,0,0,0]
+                    if length[j] + 1 == length[i]: #[1,1,1,1,1]
+                        count[i] += count[j] #[1,0,0,0,0]
         
-        max_length = max(length)
+        max_length = max(length) #2
         result = 0
         
         for i in range(n):
