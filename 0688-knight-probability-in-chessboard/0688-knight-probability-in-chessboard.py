52b03368b6f22e8401a99bdeeadfaef124f1ab1b
Soumya Polisetty <soumyapolise@umass.edu>
2023-07-21 18:05:58 -0700

    Time: 265 ms (62.71%), Space: 25.6 MB (30.27%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,40 +1,14 @@
 class Solution:
-    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        directions = [(1, 2), (1, -2), (-1, 2), (-1, -2),
-                      (2, 1), (2, -1), (-2, 1), (-2, -1)]
-        dp = [[[-1] * n for _ in range(n)] for _ in range(k + 1)]
-
-        def calculate_dp(moves, i, j):
-            # Base case
-            if moves == 0:
-                if i == row and j == column:
-                    return 1
-                else:
-                    return 0
-
-            # Check if the value has already been calculated
-            if dp[moves][i][j] != -1:
-                return dp[moves][i][j]
-
-            dp[moves][i][j] = 0
-
-            # Iterate over possible directions
-            for direction in directions:
-                prev_i = i - direction[0]
-                prev_j = j - direction[1]
-
-                # Boundary check
-                if 0 <= prev_i < n and 0 <= prev_j < n:
-                    dp[moves][i][j] += calculate_dp(moves - 1, prev_i, prev_j)
-            dp[moves][i][j] /= 8
-
-            return dp[moves][i][j]
-
-        # Calculate the total probability
-        total_probability = sum(
-            calculate_dp(k, i, j)
-            for i in range(n)
-            for j in range(n)
-        )
-
-        return total_probability
+    def knightProbability(self, N: int, K: int, row: int, column: int) -> float:
+        memo = {}
+        def dfs(i, j, p, k): 
+            if 0 <= i < N and 0 <= j < N and k < K:
+                sm = 0
+                for x, y in ((-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2)):
+                    if (i + x, j + y, k) not in memo:
+                        memo[(i + x, j + y, k)] = dfs(i + x, j + y, p / 8, k + 1)
+                    sm += memo[(i + x, j + y, k)]
+                return sm
+            else:
+                return 0 <= i < N and 0 <= j < N and p or 0
+        return dfs(row, column, 1, 0)
