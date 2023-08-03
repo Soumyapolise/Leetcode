a76e27f8598e4395f9dcb815ac6a415d15cd1a97
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-03 07:56:54 -0700

    Time: 52 ms (72.00%), Space: 16.5 MB (41.50%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,12 +2,12 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(curr):
             if len(curr) == len(nums):
-                res.append(curr[:]) #[:] creates a copy of the list by slicing - if you append the whole list, and later when you make changes by popping - the changes will be made on all the previous appends of list as well - because they were "copied by reference" - meaning, everything is saved on the same address.
-                # print(curr, curr[:])
-                return
-            for num in nums:
-                if num not in curr:
-                    curr.append(num)
+                res.append(curr[:])
+                return 
+            
+            for n in nums:
+                if n not in curr:
+                    curr.append(n)
                     backtrack(curr)
                     curr.pop()
         
