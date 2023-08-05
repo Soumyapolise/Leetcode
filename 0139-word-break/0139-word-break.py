a3d97aa516fb14e1aeac65827415cd1fb4ea148e
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-05 01:07:45 -0700

    Time: 32 ms (99.68%), Space: 16.7 MB (18.90%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        # wordDict = set(wordDict)
         
-        @lru_cache(None)
-        def dfs(start):
+        @cache
+        def checker(start):
             if start == n:
                 return True
             
             for end in range(start+1, n+1):
-                word = s[start:end]
-                if word in wordDict and dfs(end):
+                if s[start:end] in wordDict and checker(end):
                     return True
             
             return False
-            
-        return dfs(0)
+        
+        return checker(0)
         
