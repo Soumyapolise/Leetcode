1ecf987aeb48804889e74652b8708ca0149c41f7
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-11 20:40:10 -0700

    Time: 43 ms (95.25%), Space: 16.4 MB (82.98%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        # Initialize a 2D array to store the number of unique paths
+        dp = [[0] * n for _ in range(m)]
+
+        # Base case: the top-left cell is reachable if there's no obstacle there
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        # Fill in the first row: can only move right if there's no obstacle
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] if obstacleGrid[0][j] == 0 else 0
+
+        # Fill in the first column: can only move down if there's no obstacle
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] if obstacleGrid[i][0] == 0 else 0
+
+        # Fill in the rest of the array
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
+
+#     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+#         if not obstacleGrid:
+#             return 0
+
+#         res = [0]  # Using a list to hold the result
+
+#         self.dfs(obstacleGrid, 0, 0, res)
+#         return res[0]
+
+#     def dfs(self, grid, i, j, res):
+#         if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 1:
+#             return
+
+#         if i == len(grid)-1 and j == len(grid[0])-1:
+#             res[0] += 1
+#             return
+
+#         grid[i][j] = 1  # Mark the cell as visited to avoid revisiting
+
+#         self.dfs(grid, i+1, j, res) #down
+#         self.dfs(grid, i, j+1, res) #right
+
+#         grid[i][j] = 0  # Backtrack: Mark the cell as unvisited
