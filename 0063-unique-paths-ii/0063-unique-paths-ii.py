f4fe902dcb50e8de1ff1c0e13c6e360b220e00d9
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-11 20:51:50 -0700

    Time: 50 ms (79.67%), Space: 16.4 MB (58.00%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,29 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid):
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-
-        # Initialize a 2D array to store the number of unique paths
-        dp = [[0] * n for _ in range(m)]
-
-        # Base case: the top-left cell is reachable if there's no obstacle there
+        #dynamic programming solution
+        
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        
+        dp = [[0]*n for _ in range(m)]
+        
         dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
-
-        # Fill in the first row: can only move right if there's no obstacle
+        
         for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] if obstacleGrid[0][j] == 0 else 0
-
-        # Fill in the first column: can only move down if there's no obstacle
+            dp[0][j] = dp[0][j-1] if obstacleGrid[0][j] == 0 else 0
+        
         for i in range(1, m):
-            dp[i][0] = dp[i - 1][0] if obstacleGrid[i][0] == 0 else 0
-
-        # Fill in the rest of the array
+            dp[i][0] = dp[i-1][0] if obstacleGrid[i][0] == 0 else 0
+            
         for i in range(1, m):
             for j in range(1, n):
                 if obstacleGrid[i][j] == 0:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-
-        return dp[m - 1][n - 1]
-
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1] 
+        
+        return dp[-1][-1]
+        
+        
 #     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 #         if not obstacleGrid:
 #             return 0
