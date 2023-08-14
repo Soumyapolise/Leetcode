2e5d271b7dad34b5621868a245607b9d27e6e4a1
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-13 23:11:44 -0700

    Time: 457 ms (87.06%), Space: 29.6 MB (55.40%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -2,9 +2,9 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         pivot = random.choice(nums)
         
-        left = [x for x in nums if x>pivot] #because we're looking for greatest elements, so decreasing order
-        right = [x for x in nums if x<pivot]
-        mid = [x for x in nums if x==pivot]
+        left = [x for x in nums if x > pivot] #decreasing order, cuz we want the kth largest number
+        right = [x for x in nums if x < pivot]
+        mid = [x for x in nums if x == pivot]
         
         l = len(left)
         r = len(right)
@@ -12,7 +12,9 @@ class Solution:
         
         if k <= l:
             return self.findKthLargest(left, k)
-        elif k > l+m:
+        elif k > l + m:
             return self.findKthLargest(right, k-l-m)
         else:
-            return mid[0]
+            return mid[0] #cuz mid is just a list containing all the same elements
+        
+        
