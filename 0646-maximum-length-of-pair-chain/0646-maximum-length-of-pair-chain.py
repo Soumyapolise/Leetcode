1fe61071f5f0336329242375509c8a921dbe313a
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-25 19:12:25 -0700

    Time: 193 ms (94.81%), Space: 16.8 MB (89.10%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -1,20 +1,21 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-    # Sort pairs based on their ending values
-        pairs.sort(key=lambda x: x[1])
-
-        # Initialize variables to keep track of the current end value and the chain length
-        current_end = float('-inf')  # Initialize to negative infinity
-        max_length = 0
-
-        for pair in pairs:
-            if pair[0] > current_end:
-                # If the current pair can be added to the chain, update the current end value
-                current_end = pair[1]
-                max_length += 1
-
-        return max_length
-
+        pairs.sort(key = lambda x:x[1])
+        
+        res = 0
+        curr_end = float('-inf')
+        
+        ######GREEDY APPROACH ######, just taking in the first interval that you find that has smaller end point, thereby increasing range of intervals you can take in, there by increasing the "chain" as being asked by the question, to return the longest chain:
+        
+        for p in pairs:
+            if p[0] > curr_end:
+                curr_end = p[1]
+                res += 1
+        
+        return res
+        
+        
+    #DFS APPROACH - TLE
 #     def findLongestChain(self, pairs: List[List[int]]) -> int:
 #         pairs = sorted(pairs)
         
