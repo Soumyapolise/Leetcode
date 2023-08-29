f8720920a2147b0e733d2aee003a7348bc1caea9
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-28 17:30:29 -0700

    Time: 149 ms (60.24%), Space: 17.2 MB (92.17%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,26 +1,23 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         p = 0
-        d = {}
         
-        i = 0
         for i in range(len(customers)):
             if customers[i] == "Y":
                 p += 1
-        
-        d[p] = [0]
-        
+                
+        p_val = p
+        res = 0
         for j in range(1, len(customers)+1):
             if customers[j-1] == "Y":
                 p -= 1
             else:
                 p += 1
             
-            if p in d:
-                d[p] += [j]
-            else:
-                d[p] = [j]
+            if p < p_val:
+                p_val = p
+                res = j
         
-        return d[min(d.keys())][0]
+        return res
             
         
