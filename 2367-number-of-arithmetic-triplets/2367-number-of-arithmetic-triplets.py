f84aafc664653d00e0990f8a1b7b106a70e1fe4e
Soumya Polisetty <soumyapolise@umass.edu>
2023-08-30 15:24:43 -0700

    Time: 46 ms (79.59%), Space: 16.1 MB (93.00%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        d = {}
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] in d:
+                if d[nums[i]] == 1:
+                    count += 1
+                d[nums[i]] = 1
+                d[nums[i] + diff] = 1
+            else:
+                d[nums[i] + diff] = -1 #3:-1, 4:1, 7:1
+        
+        return count
