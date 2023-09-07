6eea6604d70f767da0192a16e6e252bd93ae2cee
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-06 17:10:40 -0700

    Time: 43 ms (42.77%), Space: 16.4 MB (91.05%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,31 +5,59 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        lis = []
-        if left == right:
-            return head
-        
-        while head:
-            lis.append(head.val)
-            head = head.next
-        
-        dummy = res = ListNode(0)
-        i = 0
-        while i < left-1:
-            res.next = ListNode(lis[i])
-            res = res.next
-            i += 1
-        
-        j = right-1
-        while j > left - 2:
-            res.next = ListNode(lis[j])
-            res = res.next
-            j -= 1
-        
-        k = right
-        while k < len(lis):
-            res.next = ListNode(lis[k])
-            res = res.next
-            k += 1
-        
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        # Move prev to the node just before the left position
+        for _ in range(left - 1):
+            prev = prev.next
+
+        # Initialize pointers for reversing the sublist
+        curr = prev.next
+        next_node = None
+
+        # Reverse the sublist from left to right
+        for _ in range(right - left + 1):
+            next_temp = curr.next
+            curr.next = next_node
+            next_node = curr
+            curr = next_temp
+
+        # Connect the reversed sublist back to the original list
+        prev.next.next = curr
+        prev.next = next_node
+
         return dummy.next
+
+
+
+
+#         lis = []
+#         if left == right:
+#             return head
+        
+#         while head:
+#             lis.append(head.val)
+#             head = head.next
+        
+#         dummy = res = ListNode(0)
+#         i = 0
+#         while i < left-1:
+#             res.next = ListNode(lis[i])
+#             res = res.next
+#             i += 1
+        
+#         j = right-1
+#         while j > left - 2:
+#             res.next = ListNode(lis[j])
+#             res = res.next
+#             j -= 1
+        
+#         k = right
+#         while k < len(lis):
+#             res.next = ListNode(lis[k])
+#             res = res.next
+#             k += 1
+        
+#         return dummy.next
