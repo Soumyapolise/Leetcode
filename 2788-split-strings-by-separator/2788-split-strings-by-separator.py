278d49f38b149c91b60eb841f5acbbad1ca7c300
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-08 16:41:02 -0700

    Time: 101 ms (89.96%), Space: 16.6 MB (15.63%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.py
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.py
@@ -3,15 +3,11 @@ class Solution:
         res = []
         for word in words:
             lis = word.split(separator)
-            
+            while True:
+                if "" in lis:
+                    lis.remove("")
+                else:
+                    break
             res += lis
-        
-        i = 0
-        
-        while i < len(res):
-            if res[i] == "":
-                res = res[0:i] + res[i+1:]
-            else:
-                i += 1
-                
+      
         return res
