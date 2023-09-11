e3556d1a0afba2dd1367096dbc2ff5fb827c92cd
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-11 12:11:46 -0700

    Time: 85 ms (28.79%), Space: 16.4 MB (91.52%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,20 +1,24 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         d = {}
-        
+        res = []
         for i in range(len(groupSizes)):
             if groupSizes[i] in d:
                 d[groupSizes[i]] += [i]
             else:
                 d[groupSizes[i]] = [i]
+            
+            if len(d[groupSizes[i]]) == groupSizes[i]:
+                res.append(d[groupSizes[i]])
+                d[groupSizes[i]] = []
                 
-        res = []
-        for size, item in d.items():
-            if len(item) > size:
-                k = len(item)//size
-                for i in range(k):
-                    res.append(item[i:i+size])
-            else:
-                res.append(item)
+        # res = []
+        # for size, item in d.items():
+        #     if len(item) > size:
+        #         k = len(item)//size
+        #         for i in range(k):
+        #             res.append(item[i:i+size])
+        #     else:
+        #         res.append(item)
         
         return res
