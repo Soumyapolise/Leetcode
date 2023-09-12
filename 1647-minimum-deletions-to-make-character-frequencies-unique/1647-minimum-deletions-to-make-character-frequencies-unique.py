b165675bbdec2eeea61446b498a86d62ef18ee57
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-11 18:45:53 -0700

    Time: 207 ms (31.19%), Space: 17.1 MB (62.10%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -9,11 +9,9 @@ class Solution:
         
         lis = sorted(d.values())[::-1]
         res = 0
-        print(lis)
         for i in range(1, len(lis)):
             while lis[i] > 0 and lis[i] >= lis[i-1]:
                 lis[i] -= 1
                 res += 1
 
         return res
-    #4, 1, 1, 1
