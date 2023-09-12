dfbea9facae013cd44000070aab38de8f4554a36
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-12 09:02:05 -0700

    Time: 41 ms (42.33%), Space: 16.3 MB (26.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         d = {}
+        d[0] = 1
         d[1] = 1
         
         def search(n):
             if n in d:
                 return d[n]
             
-            if n == 0:
-                return 1
             d[n] = search(n-1) + search(n-2)
         
             return d[n]
