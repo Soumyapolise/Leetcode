81dd1879322001a98363ce7501ab67b5c5293993
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-13 23:03:23 -0700

    Time: 87 ms (68.52%), Space: 16.7 MB (87.47%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,17 +1,20 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        graph = defaultdict(list)
+        d = {}
         
-        for src, dst in sorted(tickets, reverse=True):
-            graph[src].append(dst)
+        for source, destination in sorted(tickets)[::-1]:
+            if source not in d:
+                d[source] = []
+            d[source] += [destination]
+        
+        res = []
+        def dfs(city):
+            if city in d: 
+                while d[city]:
+                    dfs(d[city].pop())
+            res.append(city)
             
-        itinerary = []
-        def dfs(airport):
-            while graph[airport]:
-                dfs(graph[airport].pop())
-            itinerary.append(airport)
         
         dfs("JFK")
-        
-        return itinerary[::-1]
-        
+        return res[::-1]
+    
