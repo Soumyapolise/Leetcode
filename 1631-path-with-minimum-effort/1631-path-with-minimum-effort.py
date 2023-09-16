7edfdcb0a26bc08443071ac3571be66083b91857
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-16 10:20:09 -0700

    Time: 2603 ms (13.14%), Space: 28.7 MB (5.05%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,37 +1,23 @@
-import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        rows, cols = len(heights), len(heights[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        def bfs(threshold):
-            visited = [[False] * cols for _ in range(rows)]
-            visited[0][0] = True
-            min_heap = [(0, 0, 0)]  # (effort, row, col)
-
-            while min_heap:
-                effort, row, col = heapq.heappop(min_heap)
-
-                if row == rows - 1 and col == cols - 1:
-                    return True
-
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0 <= r < rows and 0 <= c < cols and not visited[r][c]:
-                        new_effort = max(effort, abs(heights[row][col] - heights[r][c]))
-                        if new_effort <= threshold:
-                            visited[r][c] = True
-                            heapq.heappush(min_heap, (new_effort, r, c))
-
-            return False
-
-        left, right = 0, int(1e6)
-
-        while left < right:
-            mid = (left + right) // 2
-            if bfs(mid):
-                right = mid
+        m, n = len(heights), len(heights[0])
+        neibs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
+        def dfs(LIMIT, x, y):
+            visited.add((x, y)) 
+            for dx, dy in neibs:
+                if 0<=dx+x<m and 0<=dy+y<n and (dx+x, dy+y) not in visited:
+                    if abs(heights[x][y] - heights[dx+x][dy+y]) <= LIMIT:
+                        dfs(LIMIT, dx+x, dy+y)
+        
+        beg, end = -1, max(max(heights, key=max))
+        while beg + 1 < end:
+            mid = (beg + end)//2
+            visited = set()
+            dfs(mid, 0, 0)
+            if (m - 1, n - 1) in visited:
+                end = mid
             else:
-                left = mid + 1
-
-        return left
+                beg = mid
+                
+        return end
