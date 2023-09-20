e37e58e9cfb5971208e2c2d2e9ccc9cf0d920194
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-19 21:15:30 -0700

    Time: 99 ms (61.92%), Space: 19 MB (61.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courses = defaultdict(list)
-
-        for c, p in prerequisites:
-            courses[p].append(c)
-        print(courses)
-        seen = set()
-
-        def check_cycle(cur, path):
-            if cur in path:
+        graph = [[] for _ in range(numCourses)]
+        
+        for course, prereq in prerequisites:
+            graph[course] += [prereq]
+            
+        visited = [0 for _ in range(numCourses)]
+        
+        def dfs(course):
+            if visited[course] == 1:
+                return False
+            if visited[course] == 2:
                 return True
-            if cur in seen:
-                return False
-
-            path.add(cur)
-            for child in courses[cur]:
-                if check_cycle(child, path):
-                    return True
-            path.remove(cur)
-
-            seen.add(cur)
-
-            return False
-
-        for cur in range(numCourses):
-            if check_cycle(cur, set()):
-                return False
-
+            
+            visited[course] = 1
+            for c in graph[course]:
+                if not dfs(c):
+                    return False
+            
+            visited[course] = 2
+            return True
+        
+        for course in range(numCourses):
+            if visited[course] == 0:
+                if not dfs(course):
+                    return False
+        
         return True
