9e44bdb4275a0924c9215cb4f58521dd7ebfea1f
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-23 11:11:47 -0700

    Time: 121 ms (83.69%), Space: 16.6 MB (99.11%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        words.sort(key=len) #sorting the words by their length
+        words.sort(key = len)
         
-        res = 1
         d = {}
+        res = 1
         
         for word in words:
-            d[word] = 1 #updating by minimum occurrence
+            d[word] = 1
             
             for i in range(len(word)):
-                p = word[:i] + word[i+1:] #checking without the i'th letter for predecessor -> if it's in d
-                if p in d: #if predecessor already in d, update d[word] with whatever is max as done below:
+                p = word[:i] + word[i+1:]
+                
+                if p in d:
                     d[word] = max(d[word], d[p] + 1)
-            
-            res = max(res, d[word]) #just updating res as we go for max value
+                
+            res = max(res, d[word])
         
         return res
