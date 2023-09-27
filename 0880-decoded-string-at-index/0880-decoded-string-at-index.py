2844f7cace0ff5f1b4d4ef063d0d78e08339bead
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-26 19:38:04 -0700

    Time: 31 ms (93.94%), Space: 16.3 MB (65.15%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -0,0 +1,28 @@
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        total_length = 0
+        for c in s:
+            if c.isdigit():
+                total_length *= int(c)
+            else:
+                total_length += 1
+        for c in reversed(s):
+            k %= total_length 
+            if k == 0 and c.isalpha():
+                return c
+            if c.isdigit():
+                total_length //= int(c)
+            else:
+                total_length -= 1
+        return ""
+#         res = ""
+        
+#         for ch in s:
+#             if ch.isnumeric() is False:
+#                 res += ch
+#             else:
+#                 res = res*int(ch)
+#             if len(res) >= k:
+#                 break
+        
+#         return res[k-1]
