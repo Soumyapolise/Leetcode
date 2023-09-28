5551d3c8ca946bd08c92a02348fc94ef8e871455
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-28 12:23:14 -0700

    Time: 102 ms (56.92%), Space: 16.1 MB (93.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,35 +1,18 @@
-# class Solution:
-#     def isValidSudoku(self, board: List[List[str]]) -> bool:
-#         d = {}
-        
-#         for i in range(len(board)):
-#             for j in range(len(board[0])):
-#                 element = board[i][j]
-#                 if element != ".":
-#                     if (i, element) in d or (element, j) in d or (element, i//3, j//3) in d:
-#                         return False
-#                     d[(i, element)] = 1
-#                     d[(element, j)] = 1
-#                     d[(element, i//3, j//3)] = 1
-        
-#         return True
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-
-        for i in range(9):
-            for j in range(9):
+        d = {}
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
                 element = board[i][j]
                 if element != ".":
-                    # Check the row, column, and box constraints
-                    if element in rows[i] or element in cols[j] or element in boxes[i // 3 * 3 + j // 3]:
+                    if (i, element) in d or (element, j) in d or (element, i//3, j//3) in d:
                         return False
-                    rows[i].add(element)
-                    cols[j].add(element)
-                    boxes[i // 3 * 3 + j // 3].add(element)
-
+                    d[(i, element)] = 1
+                    d[(element, j)] = 1
+                    d[(element, i//3, j//3)] = 1
+        
         return True
+
 
          
