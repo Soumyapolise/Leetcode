0b09ca295a99f598a1c6589a003133234c428e3c
Soumya Polisetty <soumyapolise@umass.edu>
2023-09-30 11:35:21 -0700

    Time: 146 ms (37.35%), Space: 17.2 MB (16.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,15 +1,16 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         d = {}
+        
         for ch in t:
             if ch not in d:
                 d[ch] = 0
             d[ch] += 1
         
-        min_window = ""
-        min_length = float('inf')
         left = 0
         missing_chars = len(d)
+        min_length = float('inf')
+        min_window = ""
         
         for right in range(len(s)):
             if s[right] in d:
@@ -18,16 +19,16 @@ class Solution:
                     missing_chars -= 1
             
             while missing_chars == 0:
-                l = right - left + 1
+                l = right - left + 1 #current window length
                 if l < min_length:
-                    min_window = s[left: right + 1]
+                    min_window = s[left: right+1]
                     min_length = l
                 
                 if s[left] in d:
                     d[s[left]] += 1
                     if d[s[left]] > 0:
                         missing_chars += 1
-                        
+                
                 left += 1
         
         return min_window
