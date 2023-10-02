89cf8d74659c9bd233b8892d5d173747260e99b4
Soumya Polisetty <soumyapolise@umass.edu>
2023-10-01 19:16:19 -0700

    Time: 197 ms (60.70%), Space: 17.2 MB (94.09%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -4,7 +4,7 @@ class Solution:
         bob = 0
         
         for i in range(1, len(colors) - 1):
-            if colors[i - 1] == colors[i] == colors[i + 1]:
+            if colors[i-1] == colors[i] and colors[i] == colors[i+1]:
                 if colors[i] == "A":
                     alice += 1
                 else:
