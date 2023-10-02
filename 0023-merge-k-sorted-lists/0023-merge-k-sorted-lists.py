db6d8e86638d9178ed9642a82ed99bc4b63f93de
Soumya Polisetty <soumyapolise@umass.edu>
2023-10-02 15:34:09 -0700

    Time: 106 ms (33.46%), Space: 19.9 MB (82.93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,21 +4,51 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        lis = []
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
         
-        for l in lists:
-            while l:
-                lis.append(l.val)
+        mid = len(lists)//2
+        l = self.mergeKLists(lists[0:mid:])
+        r = self.mergeKLists(lists[mid::])
+        return self.mergeList(l, r)
+            
+        
+    def mergeList(self, l:ListNode, r:ListNode):
+        dummy = ListNode(0)
+        head = dummy
+        while l and r:
+            if l.val<r.val:
+                dummy.next = l
                 l = l.next
+            else:
+                dummy.next = r
+                r = r.next
+            dummy = dummy.next
+        if l:
+            dummy.next = l
+        if r:
+            dummy.next = r
+        return head.next
         
-        lis = sorted(lis)
+    
+    
+    
+    ####### O(nlogn)
+        # lis = []
+        # for l in lists:
+        #     while l:
+        #         lis.append(l.val)
+        #         l = l.next
+        # lis.sort()
+        # root = ListNode(0)
+        # head = root
+        # for x in lis:
+        #     root.next = ListNode(x)
+        #     root = root.next
+        #     # root = root.next
+        # return head.next
         
-        dummy = root = ListNode(0)
-        for x in lis:
-            root.next = ListNode(x)
-            root = root.next
         
-        root.next = None
-        
-        return dummy.next
