4165047757fad2d370dbc61979e786c204835454
Soumya Polisetty <soumyapolise@umass.edu>
2023-10-28 14:08:18 -0700

    Time: 201 ms (64.76%), Space: 16.4 MB (68.52%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -5,8 +5,8 @@ class Solution:
         
         a = e = i = o = u = 1
         
-        for _ in range(n-1): ##we are calculating all the strings of length n, starting from each vowel according to the set conditions
+        for _ in range(n-1):
             a, e, i, o, u = e, a + i, a + e + o + u, i + u, a
         
-        return (a + e + i + o + u) % (10**9 + 7) ##now that we have the number of strings starting with each vowel, let's just add all the number of strings together and return it.
+        return (a + e + i + o + u) % (10**9 + 7)
     
