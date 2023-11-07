75b137c294c5585c98264a2ec1abb80d31320bab
Soumya Polisetty <soumyapolise@umass.edu>
2023-11-06 17:30:54 -0800

    Time: 705 ms (48.06%), Space: 32.1 MB (81.91%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -7,10 +7,11 @@ class Solution:
         dist.sort()
         i = 0
         time_elapsed = 0
+        
         while i < len(dist):
             if dist[i] - time_elapsed > 0:
                 res += 1
-                time_elapsed += 1
+                time_elapsed += 1 #increasing for every minute the weapon takes to recharge
                 i += 1
             else:
                 return res
