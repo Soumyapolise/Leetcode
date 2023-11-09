7532fab6bf413d73a1edc9273d12dccadca5f5ac
Soumya Polisetty <soumyapolise@umass.edu>
2023-11-09 11:02:23 -0800

    Time: 39 ms (61.63%), Space: 16.5 MB (15.22%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,25 +1,26 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         memo = {}
-
-        def recursive_decode(index):
-            if index == len(s):
+        
+        def recursive_decode(idx):
+            if idx == len(s):
                 return 1
-
-            if s[index] == '0':
+            
+            if s[idx] == "0":
                 return 0
-
-            if index in memo:
-                return memo[index]
-
-            ways = recursive_decode(index + 1)
-
-            if index + 1 < len(s) and int(s[index:index + 2]) <= 26:
-                ways += recursive_decode(index + 2)
-
-            memo[index] = ways
+            
+            if idx in memo:
+                return memo[idx]
+            
+            ways = recursive_decode(idx + 1)
+            
+            if idx + 1 < len(s) and int(s[idx:idx+2]) <= 26:
+                ways += recursive_decode(idx + 2)
+            
+            memo[idx] = ways
+            
             return ways
-
+        
         return recursive_decode(0)
 
 
