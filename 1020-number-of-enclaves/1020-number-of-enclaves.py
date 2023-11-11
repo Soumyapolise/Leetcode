d360ddb7b18bd8cfe1abfcf2fbc43a78e072b687
Soumya Polisetty <soumyapolise@umass.edu>
2023-11-11 00:39:08 -0800

    Time: 678 ms (26.63%), Space: 79.3 MB (54.33%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        sums = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    res = []
+                    self.dfs(grid, i, j, res)
+                    if res[0] == "#":
+                        sums += len(res)
+        return sums
+    
+    [[0,0,0,0,0,0,0,0,0,0],
+     [1,1,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,1,1,0,0,0,1,0,1,0],
+     [0,1,1,1,1,1,0,0,1,0],
+     [0,0,1,0,1,1,1,1,0,1],
+     [0,1,1,0,0,0,1,1,1,1],
+     [0,0,1,0,0,1,0,1,0,1],
+     [1,0,1,0,1,1,0,0,0,0],
+     [0,0,0,0,1,1,0,0,0,1]]
+    
+    def dfs(self, grid, i, j, res):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != 1:
+            return
+        
+        grid[i][j] = "#"
+        res.append("#")
+        
+        if i == 0 or i == len(grid)-1 or j == 0 or j == len(grid[0])-1:
+            res[0] = "F"
+        
+        self.dfs(grid, i + 1, j, res)
+        self.dfs(grid, i - 1, j, res)
+        self.dfs(grid, i, j + 1, res)
+        self.dfs(grid, i, j - 1, res)
