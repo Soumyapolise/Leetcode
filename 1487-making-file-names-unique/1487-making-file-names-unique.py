d0be09b50d612f17e13454826cad89cd634edf02
Soumya Polisetty <soumyapolise@umass.edu>
2023-11-15 13:32:20 -0800

    Time: 352 ms (52.91%), Space: 30.6 MB (23.84%) - LeetHub

---
+++ 1487-making-file-names-unique/1487-making-file-names-unique.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getFolderNames(self, names: List[str]) -> List[str]:
+        d = {}
+        res = []
+        
+        for name in names:
+            if name not in d:
+                res.append(name)
+                d[name] = 1 #suffix
+            else:
+                val = name + "(" + str(d[name]) + ")"
+                k = 1
+                while val in d:
+                    val = name + "(" + str(d[name] + k) + ")"
+                    k += 1
+                d[name] += 1
+                d[val] = 1
+                res.append(val)
+        
+        return res
