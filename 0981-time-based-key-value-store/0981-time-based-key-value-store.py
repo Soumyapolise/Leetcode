31d142e1603c74b101ac5b121b6963d5f01ab92a
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-01 19:36:41 -0800

    Time: 711 ms (23.08%), Space: 74.8 MB (22.63%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -10,13 +10,23 @@ class TimeMap:
             self.d[key] += [[value, timestamp]]
 
     def get(self, key: str, timestamp: int) -> str:
-        if key in self.d:
-            arr = self.d[key]
-            for v, t in arr[::-1]:
-                if t <= timestamp:
-                    return v
+        if key not in self.d:
+            return ""
+        arr = self.d[key]
+        i = 0
+        j = len(arr)
+
+        while i < j:
+            mid = i + (j-i)//2
+            if arr[mid][1] > timestamp:
+                j = mid
+            elif arr[mid][1] <= timestamp:
+                i = mid + 1
         
-        return ""
+        if j == 0:
+            return ""
+        else:
+            return arr[j-1][0]
 
 
 # Your TimeMap object will be instantiated and called as such:
