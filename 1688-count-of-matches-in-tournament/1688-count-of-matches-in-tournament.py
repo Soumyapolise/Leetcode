8988ae29d887815a7cf009655dd251d52014d13c
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-04 16:44:31 -0800

    Time: 37 ms (59.83%), Space: 16.3 MB (35.39%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,9 +1,12 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        count = 0
+#         count = 0
         
-        while n != 1:
-            count += n//2
-            n = math.ceil(n/2)
+#         while n != 1:
+#             count += n//2
+#             n = math.ceil(n/2)
         
-        return count
+#         return count
+
+##one match eliminates one team, and one team wins, so for the last winner of the game, n-1 teams need to lose, so for n-1 teams to lose - there should be n-1 matches
+        return n-1
