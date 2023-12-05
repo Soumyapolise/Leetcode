c9b43449e14575a6292444e4068bf9b3ea148aed
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-05 13:55:21 -0800

    Time: 76 ms (98.35%), Space: 16.2 MB (62.95%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,20 +3,19 @@ class Solution:
         res = 0
         n = len(s)
         
-        for i in range(n):
-            #checking for occurrence of the same character as s[i], consecutively
+        for i in range(len(s)):
             right = i
             while right < n and s[i] == s[right]:
                 right += 1
                 res += 1
             
-            #checking for side by side letters, if they are same - XaaaX
+            left = i-1
             
-            left = i-1 
             while left >= 0 and right < n and s[left] == s[right]:
                 left -= 1
                 right += 1
-                #print(s[left+1:right])
                 res += 1
         
         return res
+            
+            
