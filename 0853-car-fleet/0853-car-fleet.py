d596d059e2641ecc16fcdae2326d77a20f034182
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-06 15:00:04 -0800

    Time: 711 ms (95.28%), Space: 38 MB (64.60%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -5,6 +5,7 @@ class Solution:
         
         pairs.sort(reverse = True)
         
+        ##basically, we are going through the pairs in reverse sorted order, and figuring out for the current car and the last seen car(that is in stack), if the current car reaches the target before or at the same time as the car at stack[-1] - if it does, that means they are going to meet at some time before the target, and since the stack[-1] car is slower, that will remain in the stack (making a fleet), and we continue the process of checking the other cars. At last, the number of fleets remaining in stack is the answer!
         for p, s in pairs:
             if not stack:
                 stack.append((target - p) / s)
