4d79c37a58a589145a71e16100908e41dadde835
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-05 17:40:50 -0800

    Time: 58 ms (37.18%), Space: 16.9 MB (45.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        for ch in s:
+            if ch not in d:
+                d[ch] = 0
+            d[ch] += 1
+        
+        for ch in t:
+            if ch not in d:
+                return False
+            else:
+                if d[ch] == 0:
+                    return False
+                d[ch] -= 1
+        
+        return set(d.values()) == {0}
