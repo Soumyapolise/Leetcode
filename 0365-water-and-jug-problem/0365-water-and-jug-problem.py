48044995e358d2089f814ee75bd5135701a2777f
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-09 21:23:11 -0800

    Time: 6060 ms (5.11%), Space: 791.7 MB (5.11%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.py
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -1,15 +1,23 @@
 class Solution:
     def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
-        if targetCapacity % 2 == 1:
-            if jug1Capacity % 2 == 0 and jug2Capacity % 2 == 0:
-                return False
-        
-        if targetCapacity < math.gcd(jug1Capacity, jug2Capacity):
+        def dfs(jug1, jug2):
+            seen.add((jug1, jug2))
+            
+            if jug1 == targetCapacity or jug2 == targetCapacity or jug1 + jug2 == targetCapacity:
+                return True
+            
+            states = [(jug1Capacity, jug2), (jug1, jug2Capacity), (0, jug2), (jug1, 0), (jug1 - min(jug1, jug2Capacity - jug2), jug2 + min(jug1, jug2Capacity - jug2)), (jug1 + min(jug2, jug1Capacity - jug1), jug2 - min(jug2, jug1Capacity - jug1))]
+            
+            for j1, j2 in states:
+                if (j1, j2) not in seen:
+                    seen.add((j1, j2))
+                    if dfs(j1, j2):
+                        return True
+            
             return False
         
-        if jug1Capacity + jug2Capacity < targetCapacity:
-            return False
+        if targetCapacity % math.gcd(jug1Capacity, jug2Capacity) == 0:
+            seen = set()
+            return dfs(0, 0)
         
-        if jug1Capacity == 3 and jug2Capacity == 3 and targetCapacity == 5:
-            return False
-        return True
+        return False
