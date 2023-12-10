4b534801098035ae4ffd68796968adda7e23df26
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-09 20:38:58 -0800

    Time: 48 ms (52.14%), Space: 16.8 MB (19.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,22 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #finding pivot first
         pivot = nums.index(min(nums))
-        
         n = len(nums)
-        # nums = nums[pivot:n] + nums[0:pivot]
-        l = 0
-        r = n - 1
+        i = 0
+        j = n-1
         
-        while l <= r:
-            mid = l + (r-l)//2
-            real_mid = (mid + pivot)%n
+        while i <= j:
+            mid = i + (j-i)//2
+            real_mid = (mid + pivot) % n
+            
             if nums[real_mid] == target:
                 return real_mid
-            
-            if nums[real_mid] > target:
-                r = mid - 1
+            elif nums[real_mid] > target:
+                j = mid - 1
             else:
-                l = mid + 1
-            
+                i = mid + 1
+        
         return -1
