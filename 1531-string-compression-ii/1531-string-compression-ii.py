3c98476f0b03ef363bf8c413702d54b14c5eee9d
Soumya Polisetty <soumyapolise@umass.edu>
2023-12-27 16:12:14 -0800

    Time: 1983 ms (69.00%), Space: 17.5 MB (95.00%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,35 +1,22 @@
-def cost(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif 1 < n < 10:
-        return 2
-    elif 10 <= n < 99:
-        return 3
-    else:
-        return 4
-
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-        if s == "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" and k == 1:
-            return 3
-        
-        dp = [[float('inf')] * (k + 1) for _ in range(len(s) + 1)]
-        for i in range(k + 1):
-            dp[0][i] = 0
-        for i in range(1, len(s) + 1):
-            for j in range(k + 1):
-                if j > 0:
-                    dp[i][j] = dp[i - 1][j - 1]
-                same, diff = 0, 0
-                for p in range(i, 0, -1):
-                    if j < diff:
-                        break
-                    if s[p - 1] == s[i - 1]:
-                        same += 1
-                        dp[i][j] = min(dp[i][j], dp[p - 1][j - diff] + cost(same))
+        n = len(s)
+        dp = [[9999] * 110 for _ in range(110)]
+        dp[0][0] = 0
+
+        for i in range(1, n + 1):
+            for j in range(0, k + 1):
+                cnt, del_ = 0, 0
+                for l in range(i, 0, -1):
+                    if s[l - 1] == s[i - 1]:
+                        cnt += 1
                     else:
-                        diff += 1
-        return dp[len(s)][k]
-                
+                        del_ += 1
+
+                    if j - del_ >= 0:
+                        dp[i][j] = min(dp[i][j], dp[l - 1][j - del_] + 1 + (3 if cnt >= 100 else 2 if cnt >= 10 else 1 if cnt >= 2 else 0))
+
+                if j > 0:
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1])
+
+        return dp[n][k]
