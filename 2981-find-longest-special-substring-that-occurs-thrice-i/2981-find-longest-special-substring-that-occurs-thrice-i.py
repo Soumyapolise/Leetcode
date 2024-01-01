135509cb8634577ffd2585c9e2bbb7b541264fad
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-01 15:09:52 -0800

    Time: 48 ms (93.99%), Space: 17.5 MB (32.10%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -2,34 +2,42 @@ class Solution:
     def maximumLength(self, s: str) -> int:
         d = {}
         prev = "#"
+        count = 0
         
         for ch in s:
             #adding the current char into dictionary:
             if ch not in d:
                 d[ch] = 0
-            d[ch] += 1
-            
+            if len(ch) >= count:
+                d[ch] += 1
+                if d[ch] >= 3:
+                    count = max(count, len(ch))
+                
             #checking if previous char is equal to the current one and updating prev, if not - updating with current char
             if prev[-1] == ch:
                 prev += ch
             else:
                 prev = ch
             
+            
             #traversing through "prev" char to update values in dictionary
             for i in range(len(prev)-1):
                 string = prev[i:]
                 if string not in d:
                     d[string] = 0
-                d[string] += 1
+                if len(string) >= count:
+                    d[string] += 1
+                    if d[string] >= 3:
+                        count = max(count, len(string))
         
-        res = 0
-        for key, val in d.items():
-            if val >= 3: #occurring atleast three times
-                res = max(res, len(key))
+        # res = 0
+        # for key, val in d.items():
+        #     if val >= 3: #occurring atleast three times
+        #         res = max(res, len(key))
         
-        if res == 0: #did not find anything supporting our clause
+        if count == 0: #did not find anything supporting our clause
             return -1
         
-        return res
+        return count
             
             
