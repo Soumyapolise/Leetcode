6f9205f561124e7b48f75b4e0a605d63a7a06b26
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-12 20:37:02 -0800

    Time: 46 ms (48.64%), Space: 17.4 MB (37.53%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
+        count = 0
         self.dfs([], res, nums)
-        
         return res
     
     def dfs(self, curr, res, nums):
         if len(curr) == len(nums):
             res.append(curr)
             return
-
+        
         for n in nums:
             if n not in curr:
                 self.dfs(curr + [n], res, nums)
