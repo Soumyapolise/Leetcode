8cd43721a1dafd27ad9ec999cfa098d5338533c2
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-13 17:35:22 -0800

    Time: 207 ms (38.47%), Space: 18.7 MB (5.26%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,20 +1,22 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        # word1.sort()
+        # word2.sort()
+        
         if len(word1) != len(word2):
             return False
+        d2 = {}
+        d = {}
+        for ch in word1:
+            if ch not in d:
+                d[ch] = 0
+            d[ch] += 1
+            
+        for ch in word2:
+            if ch not in d2:
+                d2[ch] = 0
+            
+            d2[ch] += 1
         
-        d1, d2 = {}, {}
+        return sorted(d.keys()) == sorted(d2.keys()) and sorted(d.values()) == sorted(d2.values()) ##as long as the keys are same and values (doesn't have to respected to keys mentioned before) are same as well - we can just swap them around to achieve the same strings.
         
-        for w in word1:
-            if w in d1:
-                d1[w] += 1
-            else:
-                d1[w] = 1
-        
-        for w in word2:
-            if w in d2:
-                d2[w] += 1
-            else:
-                d2[w] = 1
-                
-        return sorted(d1.keys()) == sorted(d2.keys()) and sorted(d1.values()) == sorted(d2.values())
