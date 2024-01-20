ff1f94dc26c29a6ad2077be53fc4fb00bdb4863d
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-20 13:25:59 -0800

    Time: 451 ms (96.28%), Space: 31.4 MB (88.74%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def backtrack(s, i, arr, res):
+            if i == len(s):
+                res.append(arr[:])
+                return
+            
+            for j in range(i, len(s)):
+                sub = s[i:j+1]
+                if sub == sub[::-1]:
+                    arr.append(sub)
+                    backtrack(s, j+1, arr, res)
+                    arr.pop()
+        
+        
+        res = []
+        backtrack(s, 0, [], res)
+        return res
