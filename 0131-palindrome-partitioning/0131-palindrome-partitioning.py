4040843cd306fa4977c79cef2f97717aca31f90e
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-20 13:29:30 -0800

    Time: 458 ms (91.66%), Space: 30.3 MB (98.67%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -6,13 +6,13 @@ class Solution:
                 return
             
             for j in range(i, len(s)):
-                sub = s[i:j+1]
+                sub = s[i:j+1]       
                 if sub == sub[::-1]:
                     arr.append(sub)
                     backtrack(s, j+1, arr, res)
                     arr.pop()
-        
-        
+            
         res = []
         backtrack(s, 0, [], res)
         return res
+                          
