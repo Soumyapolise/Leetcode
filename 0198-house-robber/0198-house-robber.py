706adb6863038d006c05229a6d6f679b5636c39e
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-20 20:46:27 -0800

    Time: 40 ms (46.58%), Space: 16.5 MB (60.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,58 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        return self.dfs(nums, 0, memo)
-
-    def dfs(self, nums, i, memo):
-        # If we have reached or exceeded the end of the array, return 0
+        if len(nums) == 1:
+            return nums[0]
+        
+        sums = 0
+        return self.dfs(0, nums, {})
+        
+        
+    def dfs(self, i, nums, memo):
         if i >= len(nums):
             return 0
-
-        # If the result for this index is already computed, return it
+        
         if i in memo:
             return memo[i]
-
-        # Calculate the maximum amount that can be robbed starting from this house
-        # We have two choices: rob this house and move two steps ahead, or skip this house and move one step ahead
-        max_amount = max(nums[i] + self.dfs(nums, i + 2, memo), self.dfs(nums, i + 1, memo))
-
-        # Store the result in the memo table
-        memo[i] = max_amount
-
-        return max_amount
         
-#         lis = []
-#         res = 0
-#         memo = {}
-#         for i in range(len(nums)):
-#             res = 0
-#             self.dfs(nums, i, res, lis)
+        memo[i] = max(nums[i] + self.dfs(i+2, nums, memo), self.dfs(i+1, nums, memo))
         
-#         return max(lis)
-        
-        
-#     def dfs(self, nums, i, res, lis):
-#         if i >= len(nums):
-#             return
-        
-        
-#         if i == len(nums)-1 or i == len(nums)-2:
-#             lis.append(res + nums[i])
-#             return
-        
-#         idx = []
-#         start = i
-#         while start < len(nums):
-#             if start != i and start != (i+1):
-#                 idx.append(start)
-#             start += 1
-        
-#         if idx == []:
-#             lis.append(res + nums[i])
-#             return
-        
-#         for j in idx:
-#             self.dfs(nums, j, res + nums[i], lis)
-        
-        
-        
+        return memo[i]
