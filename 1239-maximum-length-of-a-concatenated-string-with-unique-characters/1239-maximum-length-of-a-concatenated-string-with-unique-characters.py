4297ee037df16e6433a5b81483b54ac87c56a3d1
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-23 11:14:44 -0800

    Time: 98 ms (50.79%), Space: 16.6 MB (75.18%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -3,21 +3,23 @@ class Solution:
         res = []
         
         def backtrack(arr, res, i, curr):
-            if i == len(arr):
-                res.append(curr[:])
-                return
+            nonlocal l
+            
+            l = max(l, len("".join(curr)))
             
             for j in range(i, len(arr)):
                 val = "".join(curr)
                 if len(val) + len(arr[j]) == len(set(val + arr[j])):
                     curr.append(arr[j])
-                    res.append(curr[:])
+                    # res.append(curr[:])
                     
                     backtrack(arr, res, j+1, curr)
                     curr.pop()
         l = 0            
         backtrack(arr, res, 0, [])
-        for x in res:
-            l = max(l, len("".join(x)))
+#         print(res)
+        
+#         for x in res:
+#             l = max(l, len("".join(x)))
             
         return l
