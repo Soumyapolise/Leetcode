7744925cf99b5b7e587db9d47c77fb3ab1a43f7e
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-26 13:01:26 -0800

    Time: 89 ms (69.36%), Space: 21.9 MB (47.69%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -1,24 +1,3 @@
-# class Solution:
-#     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-#         memo = {}
-#         mod = 10**9 + 7
-#         def dfs(i, j, memo, num_moves):
-#             if i < 0 or j < 0 or i >= m or j >= n:
-#                 if num_moves <= maxMove:
-#                     if (i,j) in memo:
-#                         return memo[(i, j)]%mod
-#                     else:
-#                         return 1
-                
-#             if num_moves >= maxMove:
-#                 return 0
-            
-#             memo[(i, j)] = (dfs(i+1, j, memo, num_moves+1)%mod + dfs(i-1, j, memo, num_moves+1)%mod + dfs(i, j+1, memo, num_moves+1)%mod + dfs(i, j-1, memo, num_moves+1)%mod)%mod
-            
-#             return memo[(i, j)]%mod
-        
-#         return dfs(startRow, startColumn, memo, 0)
-
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         memo = {}
@@ -31,7 +10,7 @@ class Solution:
             if i < 0 or j < 0 or i >= m or j >= n:
                 return 1
             
-            if (i, j, num_moves) in memo:
+            if (i, j, num_moves) in memo:  ##storing i, j and num_moves, cuz we wanna keep track of how many moves we CAN make going forward as well. not just the i and j as position.
                 return memo[(i, j, num_moves)]%mod
             
             memo[(i, j, num_moves)] = (dfs(i+1, j, num_moves+1)%mod + dfs(i-1, j, num_moves+1)%mod + dfs(i, j+1, num_moves+1)%mod + dfs(i, j-1, num_moves+1)%mod) % mod
