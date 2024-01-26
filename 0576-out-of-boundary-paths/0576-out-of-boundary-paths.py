9ec8aefca7b4a21876efedad1d8d4b2036106487
Soumya Polisetty <soumyapolise@umass.edu>
2024-01-26 13:00:25 -0800

    Time: 94 ms (62.72%), Space: 21.9 MB (46.24%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -27,17 +27,15 @@ class Solution:
         def dfs(i, j, num_moves):
             if num_moves > maxMove:
                 return 0
-
+            
             if i < 0 or j < 0 or i >= m or j >= n:
                 return 1
-
+            
             if (i, j, num_moves) in memo:
-                return memo[(i, j, num_moves)] % mod
-
-            result = (dfs(i+1, j, num_moves+1) % mod + dfs(i-1, j, num_moves+1) % mod +
-                      dfs(i, j+1, num_moves+1) % mod + dfs(i, j-1, num_moves+1) % mod) % mod
-
-            memo[(i, j, num_moves)] = result
-            return result
+                return memo[(i, j, num_moves)]%mod
+            
+            memo[(i, j, num_moves)] = (dfs(i+1, j, num_moves+1)%mod + dfs(i-1, j, num_moves+1)%mod + dfs(i, j+1, num_moves+1)%mod + dfs(i, j-1, num_moves+1)%mod) % mod
+            
+            return memo[(i, j, num_moves)]
 
         return dfs(startRow, startColumn, 0)
