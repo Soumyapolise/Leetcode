70695e3212719af7fde5caceca8267501a6a9378
Soumya Polisetty <soumyapolise@umass.edu>
2024-02-07 18:40:33 -0800

    Time: 2391 ms (61.22%), Space: 16.6 MB (81.54%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,27 +1,39 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        memo = {}
+#         memo = {}
         
-        def dfs(n):
-            nonlocal memo
+#         def dfs(n):
+#             nonlocal memo
             
-            if n == 0:
-                return 0
+#             if n == 0:
+#                 return 0
             
-            if n < 0:
-                return float('inf')
-            if n in memo:
-                return memo[n]
+#             if n < 0:
+#                 return float('inf')
+#             if n in memo:
+#                 return memo[n]
             
-            min_count = float('inf')
+#             min_count = float('inf')
             
-            for i in range(1, int(n**(0.5)) + 1):
-                val = n - i**2
-                min_count = min(min_count, dfs(val) + 1)
+#             for i in range(1, int(n**(0.5)) + 1):
+#                 val = n - i**2
+#                 min_count = min(min_count, dfs(val) + 1)
             
-            memo[n] = min_count
-            return min_count
+#             memo[n] = min_count
+#             return min_count
         
-        dfs(n)
+#         dfs(n)
         
-        return memo[n]
+#         return memo[n]
+
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        
+        # Iterate over numbers from 1 to n
+        for i in range(1, n + 1):
+            # Iterate over perfect squares less than or equal to i
+            for j in range(1, int(i**0.5) + 1):
+                # Update the minimum count for i
+                dp[i] = min(dp[i], dp[i - j**2] + 1)
+        
+        return dp[n]
