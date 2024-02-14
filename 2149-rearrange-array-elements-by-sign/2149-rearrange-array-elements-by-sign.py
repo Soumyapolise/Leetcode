1b0400acf35ffab229333b1c3ce42e9a4e2883cc
Soumya Polisetty <soumyapolise@umass.edu>
2024-02-14 12:28:33 -0800

    Time: 1047 ms (34.49%), Space: 47.6 MB (57.06%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -2,38 +2,13 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [0]*n
-        i = 0
-        j = 0
+        pos = 0
+        neg = 1
         for k in range(n):
-            if k % 2 == 0:
-                while i < n and nums[i] < 0:
-                    i += 1
-                res[k] = nums[i]
-                i += 1
+            if nums[k] > 0:
+                res[pos] = nums[k]
+                pos += 2
             else:
-                while j < n and nums[j] > 0:
-                    j += 1
-                res[k] = nums[j]
-                j += 1
+                res[neg] = nums[k]
+                neg += 2
         return res
-#         p, n = 0, 1
-#         i, j = 0, 0
-            
-#         length = len(nums)
-#         while p < length and n < length:
-#             while i < length and nums[i] < 0:
-#                 i += 1
-            
-#             while j < length and nums[j] > 0:
-#                 j += 1
-                
-#             if p < length and nums[p] < 0:
-#                 nums[p], nums[j] = nums[j], nums[p]
-            
-#             if n < length and nums[n] > 0:
-#                 nums[n], nums[i] = nums[i], nums[n]
-                
-#             p += 2
-#             n += 2
-            
-#         return nums
