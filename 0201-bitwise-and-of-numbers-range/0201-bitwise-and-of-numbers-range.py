d1385582795a31c82afed79508b207b7c719c234
Soumya Polisetty <soumyapolise@umass.edu>
2024-02-20 16:59:40 -0800

    Time: 45 ms (86.48%), Space: 16.5 MB (81.69%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,8 +1,14 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         cnt = 0
+        
+        #finding the common (most-significant) bits in both left and right numbers.
         while left != right:
             left >>= 1
             right >>= 1
             cnt += 1
+            
+        #eg 101, 111 -> left = right = 1, cnt = 2, then adding cnt number of 0's, to depict the number of non-common occurences, since this is AND operation, unless all are same - we'll get that result else, 0, so we'll just add cnt many 0's to our left(or right,  both are equal at this point)
         return left << cnt
+    
+        
