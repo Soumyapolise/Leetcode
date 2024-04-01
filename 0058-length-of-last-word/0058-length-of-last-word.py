31f9e5c963193a7fe868f9cf69833a5a21f4bb3b
Soumya Polisetty <soumyapolise@umass.edu>
2024-04-01 14:58:03 -0700

    Time: 37 ms (41.35%), Space: 16.6 MB (27.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == " ":
+                if count == 0:
+                    continue
+                else:
+                    return count
+            count += 1
+        return count
